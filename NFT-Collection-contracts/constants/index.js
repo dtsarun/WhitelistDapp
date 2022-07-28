eab5bbc82b09f6806d8c4489174fb75ae8bbc556
Arunkumar Natarajan <dtsarun@gmail.com>
2022-07-28 14:16:54 +0530

    NFT-Collection

---
+++ NFT-Collection-contracts/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0x00de84E9994aefef7dA0aD6d7c627d449dc77111";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
